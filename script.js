15e8a9b92a5761d57f6b66d62e4e382730589c55
Melanie Roberts <robertsmelaniecc@gmail.com>
2025-04-19 16:55:57 -0400

    first

---
+++ script.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const nodeMailer = require('nodemailer');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+
+const app = express();
+app.use(bodyParser.uurlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.post('/contact', async (require, res) => {
+    const { name, email, message } = req.body;
+
+    // Basic Validation
+    if (!name || !email || !message) {
+        return res.status(400).send('All fields are required.');
+    }
+
+    // Nodemailer setup
+    const transporter = nodeMailer.createTransport({
+        service: 'gmail', // or use any SMTP Server
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS
+        }
+    });
+
+    const mailOptions = {
+        from: email,
+        to: process.env.EMAIL_USER,
+        subject: `Contact form submission from ${name}`,
+        text: message
+    };
+
+    try {
+        await transporter.sendMail(mailOptions);
+        res.send('Email sent successfully!');
+    } catch (error) {
+        res.status(500).send('Error sending email.');
+
+    }
+});
+
+app.listen(3000, () => {
+    console.log('Server running on port 3000');
+});
+
